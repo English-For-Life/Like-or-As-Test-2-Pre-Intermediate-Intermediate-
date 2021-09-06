b6c95ab39acd4f5811ee712196a7a9e2af0f5824
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-09-06 20:09:56 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Suddenly there was a terrible noise. It was ..... a bomb exploding.",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"She's a really good swimmer. She swims ..... a fish.",
+        options:["as", "like"],
+        answer: 1
+    },
+    {
+        q:"He's been learning English for a few years but he still speaks ..... a beginner.",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"My feet are really cold. They are ..... blocks of ice.",
+        options:["are", "like"],
+        answer: 1
+    },
+    {
+        q:"Margaret once had a part-time job ..... a tourist guide.",
+        options:["as", "like"],
+        answer: 0
+    },
+    {
+        q:"We don't need all the bedrooms in the house, so we use one of them ..... a study.",
+        options:["like", "as"],
+        answer: 1
+    },
+    {
+        q:"His house is full of lots of interesting things. It's ..... a museum.",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"Have you ever worked ..... a labourer on a building site?",
+        options:["like", "as"],
+        answer: 1
+    },
+    {
+        q:"The news that he was getting married came ..... a complete surprise to me.",
+        options:["as", "like"],
+        answer: 0
+    },
+    {
+        q:"He's 35 but he sometimes behaves ..... a child.",
+        options:["as", "like"],
+        answer: 1
+    }
+
+
+
+]
